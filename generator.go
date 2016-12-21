d65317e199e2e0e7123966d014b00cef6aeecb95
Nauris Sadovskis <nauris.sadovskis@bynd.com>
2016-12-21 18:55:38 +0000

    somewhat working version of 'init'

--- generator.go
+++ generator.go
@@ -3,6 +3,8 @@ package main
 import "os"
 import "fmt"
 import "bufio"
+import "strings"
+import "io/ioutil"
 
 func help() {
   fmt.Println("This is help. Run 'generator init'")
@@ -29,10 +31,9 @@ func prompt(filename string) {
   generate(filename)
 }
 
-func initialize() {
-  filename := "file.txt"
+func initialize(filename string) {
   if _, err := os.Stat(filename); os.IsNotExist(err) {
-    os.Create(filename)
+    generate(filename)
     fmt.Println(filename + " initialized.")
   } else {
     prompt(filename)
@@ -40,45 +41,64 @@ func initialize() {
 }
 
 func generate(filename string) {
-  // make template from a list
-  // save it as template
-  variables := [6]string{"aws_region", "aws_account", "aws_test", "aws_what", "environment", "dev"}
-  template := make([]string, 6)
+
+  variables := []string{"aws_region",
+                        "aws_account",
+                        "environment",
+                        "cost_code",
+                        "project",
+                        "owner",
+                        "test_user"}
+
+  template := make([]string, len(variables))
+
   for i := 0; i < len(variables); i++ {
-    res := "variable " + variables[i] + " {\n\tdescription = \"fffffffff\"\n}"
+    res := "variable " + variables[i] + " {\n\tdescription = \"<modify>\"\n}\n"
     template[i] = res
   }
 
-  slice := template[0:len(template)]
+  // array has to be converted before parsing to byte[0]
+  output := "\x00" + strings.Join(template, "\x20\x00") // x20 = space and x00 = null
 
-  fmt.Println(template)
-  fmt.Println(slice)
-  raw := slice[0:len(slice)]
-  fmt.Println(raw)
   file, err := os.Create(filename)
   if err != nil {
       fmt.Println("Cannot create file", err)
+      os.Exit(1)
   }
+
   defer file.Close()
-  // fmt.Fprintf(file, slice)
-  d2 := []byte("zen")
+
+  d2 := []byte(output)
   n2, err := file.Write(d2)
   fmt.Printf("wrote %d bytes\n", n2)
   if err != nil {
   	fmt.Println(err)
-    os.Exit(1)
-  }
 
+  }
+}
+
+func validate(filename string) {
+  file, err := os.Open(filename) // For read access.
+  if err != nil {
+  	fmt.Println(err)
+  }
+  b, err := ioutil.ReadFile(file)
+  fmt.Print(b)
+  // str := byte(file) // convert content to a 'string'
+  //
+  // fmt.Println(file) // print the content as a 'string'
 }
 
 func main() {
 
     // version := "0.0.1alpha"
-
+    filename := "variables.tf"
     if len(os.Args) == 1 {
       help()
     } else if os.Args[1] == "init" {
-      initialize()
+      initialize(filename)
+    } else if os.Args[1] == "validate" {
+      validate(filename)
     } else {
         fmt.Println(len(os.Args))
     }
